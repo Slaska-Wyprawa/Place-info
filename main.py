f75339fced14930813f46f111cd9aee20e8bf850
Paszymaja <36695377+Paszymaja@users.noreply.github.com>
2021-08-04 20:11:38 +0200

    place name and adress

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
 from OSMPythonTools.api import Api
+from OSMPythonTools.nominatim import Nominatim
 from fastapi import FastAPI, HTTPException
 
 OSMApi = Api()
 app = FastAPI()
+nominatim = Nominatim()
 
 places = {
-    "sztolnia": 165513542
+    "sztolnia": {
+        "id": "421787802",
+        "lat": 50.29566,
+        "lon": 18.80620
+    }
 }
 
 
-@app.get("/place/{name}")
-async def get_place_name(name):
-    if name in places:
-        place = OSMApi.query(f"way/{places.get(name)}")
+@app.get("/place/{place}")
+async def get_info(place):
+    if place in places:
+        place = OSMApi.query(f"way/{places.get(place).get('id')}")
         return place.tags()
+    else:
+        raise HTTPException(status_code=404, detail="Place not found")
+
+
+@app.get("/address/{place}")
+async def get_address(place):
+    if place in places:
+        place = nominatim.query(places.get(place).get('lat'), places.get(place).get('lon'), reverse=True)
+        return place
     else:
         raise HTTPException(status_code=404, detail="Place not found")
--- test_main.py
+++ test_main.py
@@ -5,16 +5,36 @@ from .main import app
 client = TestClient(app)
 
 
-def test_get_place_name():
+def test_get_info():
     response = client.get("/place/sztolnia")
     assert response.status_code == 200
-    assert response.json() == {"heritage": "2", "heritage:operator": "nid",
-                               "name": "Główna Kluczowa Sztolnia Dziedziczna", "name:de": "Hauptschlüssel-Erbstollen",
-                               "name:pl": "Główna Kluczowa Sztolnia Dziedziczna", "ref:nid": "A/220/07 z 11.12.2007",
-                               "tourism": "attraction", "wikidata": "Q11708267",
-                               "wikipedia": "pl:Główna Kluczowa Sztolnia Dziedziczna"}
+    assert response.json() == {
+        "addr:city": "Zabrze",
+        "addr:housenumber": "410",
+        "addr:postcode": "41-800",
+        "addr:street": "Wolności",
+        "name": "Sztolnia Królowa Luiza",
+        "operator": "Muzeum Górnictwa Węglowego w Zabrzu",
+        "tourism": "theme_park",
+        "website": "https://www.sztolnialuiza.pl/"
+    }
 
 
 def test_get_wrong_place_name():
     response = client.get("/place/test_dom")
     assert response.status_code == 404
+
+
+def test_get_address():
+    response = client.get("/address/sztolnia")
+    assert response.status_code == 200
+    assert response.json() == {'_json': [
+        {'place_id': 139221661, 'licence': 'Data © OpenStreetMap contributors, ODbL 1.0. https://osm.org/copyright',
+         'osm_type': 'way', 'osm_id': 202692683, 'lat': '50.29570875', 'lon': '18.806103131597553',
+         'display_name': '408, Wolności, Zaborze Południe, Zabrze, Górnośląsko-Zagłębiowska Metropolia, województwo '
+                         'śląskie, 41-806, Polska',
+         'address': {'house_number': '408', 'road': 'Wolności', 'city_district': 'Zaborze Południe', 'city': 'Zabrze',
+                     'state_district': 'Górnośląsko-Zagłębiowska Metropolia', 'state': 'województwo śląskie',
+                     'postcode': '41-806', 'country': 'Polska', 'country_code': 'pl'},
+         'boundingbox': ['50.2954637', '50.2959421', '18.8055446', '18.8066599']}], '_queryString': 'reverse',
+                               '_params': {'lat': 50.29566, 'lon': 18.8062}}
